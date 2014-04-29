f06981da6fee4f146d775d68fef0dcb224e7b8c7
nimmerman <immerman@umich.edu>
2014-04-28 21:20:48 -0400

    test1

---
+++ test.cpp
@@ -0,0 +1,28 @@
+// Nathan Immerman
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void printGreeting(int population) {
+  if (population == 0) {
+    cout << "And then there were none..." << endl;
+  }
+  else if (population < 100) {
+    cout << "It's a small world after all!" << endl;
+  }
+  else {
+    cout << "It's a big world out there!" << endl;
+  }
+}
+
+int main() {
+  cout << "Hello World!" << endl;
+  cout << "How many people are there?" << endl;
+  int pop;  
+  cin >> pop;
+  printGreeting(pop);
+  cout << "Goodbye World!" << endl;
+
+  return 0;
+}
